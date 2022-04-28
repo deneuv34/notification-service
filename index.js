0f07b59010196e2105e54f0e3d3ccc47026e4665
Rangga Adhitya <deneuv3.4@gmail.com>
2022-04-28 14:38:08 +0700

    update env

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const { default: helmet } = require('helmet');
 const { APP_PORT } = require('./lib/config');
-const authentication = require('./middlewares/authentication');
-const errorHandler = require('./middlewares/errorHandler');
 const v1Routes = require('./routes/v1');
 
 // deepcode ignore HardcodedSecret: <please specify a reason of ignoring this>
@@ -11,7 +9,6 @@ const app = express();
 // middlewares setup
 app.use(helmet());
 app.use(express.json());
-app.use(errorHandler)
 // route setup
 app.use('/health', (_req, res) => res.send('OK'));
 app.use('/v1', v1Routes)
--- middlewares/errorHandler.js
+++
@@ -1,5 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  res.status(500).json({ error: error.message });
-}
-
-module.exports = errorHandler;
